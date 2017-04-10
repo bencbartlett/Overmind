bef4b6a616ddbcf42d6ede08003b4dcf21c64687
Ben Bartlett <benjamincbartlett@gmail.com>
2017-04-09 20:32:45 -0700

    Deleted extraneous files

--- globals.js
+++
@@ -1,8 +0,0 @@
-global.deref = function (ref) {
-    return Game.getObjectById(ref) ||
-        Game.flags[ref] ||
-        Game.creeps[ref] ||
-        Game.spawns[ref] ||
-        Game.rooms[ref] ||
-        null;
-};
