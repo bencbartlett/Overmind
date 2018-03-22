8620ad1f8ae7c5d42142fb6f2fd630ad6d155eb9
Ben Bartlett <benjamincbartlett@gmail.com>
2018-03-22 14:48:33 -0700

    Switched to personal version of typed-screeps

--- src/declarations/index.d.ts
+++ src/declarations/index.d.ts
@@ -13,45 +13,12 @@ declare namespace NodeJS {
 	}
 }
 
+
+// If TS2451 gets thrown, change "declare let Game: Game;" to "declare var Game: Game;"
+// in typed-screeps index.d.ts file. (See issue #61 until the package is updated)
 interface Game {
 	zerg: { [name: string]: any };
 	directives: { [name: string]: any };
-	profiler: ScreepsGameProfiler;
-}
-
-
-interface ScreepsGameProfiler {
-
-	profile(ticks: number, functionFilter?: string): void;
-
-	stream(ticks: number, functionFilter?: string): void;
-
-	email(ticks: number, functionFilter?: string): void;
-
-	background(functionFilter?: string): void;
-
-	output(lineCount?: number): void;
-
-	reset(): void;
-
-	restart(): void;
-}
-
-interface ScreepsProfilerStatic {
-
-
-}
-
-declare module 'screeps-profiler' {
-	export function enable(): void;
-
-	export function wrap(callback: Function): Function;
-
-	export function registerClass(constructor: Function, className: string): void;
-
-	export function registerObject(object: any, objectName: string): void;
-
-	export function registerFN(fn: Function, fnName?: string): Function;
 }
 
 
