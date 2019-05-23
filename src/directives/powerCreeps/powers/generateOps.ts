2b207b60a77bf9083de4f25eb8a0eadb470692ff
Matthew Roy <mroy@linkedin.com>
2019-05-23 13:17:53 -0700

    More work done

--- src/directives/powerCreeps/powers/generateOps.ts
+++ src/directives/powerCreeps/powers/generateOps.ts
@@ -1,5 +1,6 @@
 import {profile} from "../../../profiler/decorator";
 import {Power} from "./genericPower";
+import {log} from "../../../console/log";
 
 export const powerId = PWR_GENERATE_OPS;
 
@@ -7,7 +8,21 @@ export const powerId = PWR_GENERATE_OPS;
  * An abstract class for encapsulating power creep power usage.
  */
 @profile
-export abstract class GenerateOps extends Power{
+export abstract class GenerateOps extends Power {
 
-	run() {}
+	operatePower() {
+		if (this.powerCreep.carry.ops && this.powerCreep.carry.ops > (this.powerCreep.carryCapacity * 0.9)) {
+			const storage = this.powerCreep.room!.storage;
+			if (!storage) {
+				log.error(`Ops power creep with no storage`);
+			} else {
+				this.powerCreep.moveTo(this.powerCreep.room!.storage!.pos);
+				this.powerCreep.transfer(storage, RESOURCE_OPS, this.powerCreep.carry.ops);
+			}
+		} else {
+			return this.powerCreep.usePower(powerId);
+		}
+		return ERR_TIRED;
+
+	}
 }
--- src/directives/powerCreeps/powers/genericPower.ts
+++ src/directives/powerCreeps/powers/genericPower.ts
@@ -8,10 +8,50 @@ import {powerId} from "./generateOps";
 export abstract class Power {
 	static powerId: PowerConstant;
 
-	canRunPower(pc: PowerCreep) {
-		const power = pc.powers[powerId];
+	_target: { 					// Data for the target the task is directed to:
+		ref: string; 				// Target id or name
+		_pos: ProtoPos; 			// Target position's coordinates in case vision is lost
+	};
+
+
+	_powerCreep: {
+		name: string;
+	};
+
+	/**
+	 * Dereferences the Task's target
+	 */
+	get target(): RoomObject | null {
+		return deref(this._target.ref);
+	}
+
+	canRunPower() {
+		const power = this.powerCreep.powers[powerId];
 		return power && power.level > 0 && power.cooldown == 0;
 	}
 
-	run() {}
+	/**
+	 * Return the wrapped creep which is executing this task
+	 */
+	get powerCreep(): PowerCreep { // Get task's own creep by its name
+		// Returns zerg wrapper instead of creep to use monkey-patched functions
+		return Game.powerCreeps[this._powerCreep.name];
+	}
+
+	/**
+	 * Set the creep which is executing this task
+	 */
+	set powerCreep(pc: PowerCreep) {
+		this._powerCreep.name = pc.name;
+	}
+
+	run() {
+		if (this.canRunPower()) {
+			this.operatePower()
+		}
+	}
+
+	operatePower() {
+
+	}
 }
