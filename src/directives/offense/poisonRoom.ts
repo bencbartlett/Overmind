48fc6ba8592a66daa8b7463125aed46349ce63b0
hasoon hasoon <hasoon1@yahoo.com>
2019-06-05 07:40:25 +0300

    from dangerouHostile to hostiles

--- src/directives/offense/poisonRoom.ts
+++ src/directives/offense/poisonRoom.ts
@@ -60,7 +60,7 @@ export class DirectivePoisonRoom extends Directive {
 	spawnMoarOverlords() {
 		if(!this.pos.isVisible){
 			this.overlords.scout = new StationaryScoutOverlord(this);	
-		} else if(this.room!.dangerousPlayerHostiles.length == 0 && !this.isPoisoned()){
+		} else if(this.room!.playerHostiles.length == 0 && !this.isPoisoned()){
 			this.overlords.claim = new ClaimingOverlord(this);
 			this.overlords.roomPoisoner = new RoomPoisonerOverlord(this);
 		}
@@ -75,7 +75,7 @@ export class DirectivePoisonRoom extends Directive {
 		if(this.room && this.room.controller && this.room.controller.reservation && this.room.controller.reservation.ticksToEnd > 500){
 			DirectiveControllerAttack.createIfNotPresent(this.room.controller.pos,'room');
 		}
-		if(this.room && this.room.dangerousPlayerHostiles.length > 0){	
+		if(this.room && this.room.playerHostiles.length > 0){	
 			DirectiveOutpostDefense.createIfNotPresent(new RoomPosition(25,25,this.room.name),'room');
 		}
 	}
