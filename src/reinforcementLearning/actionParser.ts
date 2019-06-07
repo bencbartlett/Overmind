301d7a9c0b8e22b704c2ca8c65170872d0dd091c
Ben Bartlett <benjamincbartlett@gmail.com>
2019-06-07 15:28:14 -0700

    enable running RL mode with access to Zerg/CombatZerg methods

--- src/reinforcementLearning/actionParser.ts
+++ src/reinforcementLearning/actionParser.ts
@@ -1,15 +1,27 @@
+/*
+
+ _____  _    _ _______  ______ _______ _____ __   _ ______
+|     |  \  /  |______ |_____/ |  |  |   |   | \  | |     \
+|_____|   \/   |______ |    \_ |  |  | __|__ |  \_| |_____/
+....... R E I N F O R C E M E N T   L E A R N I N G .......
+
+*/
+
+import {CombatZerg} from '../zerg/CombatZerg';
 import {RL_TRAINING_VERBOSITY} from '../~settings';
 
 export const RL_ACTION_SEGMENT = 70;
 
 export type RLAction =
 	['move', DirectionConstant]
-	| ['moveTo', string]
+	// | ['moveTo', string]
+	| ['goTo', string]
 	| ['attack', string]
 	| ['rangedAttack', string]
 	| ['rangedMassAttack', null]
 	| ['heal', string]
-	| ['rangedHeal', string];
+	| ['rangedHeal', string]
+	| ['noop', null];
 
 /**
  * The ActionParser provides a line of direct interaction for the external Python optimizers to control
@@ -18,48 +30,84 @@ export type RLAction =
 export class ActionParser {
 
 	/**
+	 * Parse an individual action from its serialized format and command the actor to execute it.
+	 * Returns whether the action was valid.
+	 */
+	private static parseAction(actor: CombatZerg, action: RLAction): boolean {
+
+		const [command, id] = action;
+		const targ: RoomObject | null = typeof id == 'string' ? Game.getObjectById(id) : null;
+
+		switch (command) {
+			case 'move':
+				actor.move(<DirectionConstant>id);
+				break;
+			// case 'moveTo':
+			// 	if (targ) creep.moveTo(targ);
+			// 	break;
+			case 'goTo':
+				if (targ) actor.goTo(targ);
+				break;
+			case 'attack':
+				if (targ) actor.attack(<Creep>targ);
+				break;
+			case 'rangedAttack':
+				if (targ) actor.rangedAttack(<Creep>targ);
+				break;
+			case 'rangedMassAttack':
+				actor.rangedMassAttack();
+				break;
+			case 'heal':
+				if (targ) {
+					actor.heal(<Creep>targ);
+				} else if (typeof id != 'string') {
+					actor.heal(actor);
+				}
+				break;
+			case 'rangedHeal':
+				if (targ) actor.rangedHeal(<Creep>targ);
+				break;
+			case 'noop':
+				break;
+			default:
+				console.log(`[${Game.time}] Invalid command: ${command}!`);
+				return false;
+		}
+		return true;
+	}
+
+	/**
 	 * Determine the list of actions for each Zerg to perform
 	 */
-	private static parseActions(serializedActions: { [creepName: string]: RLAction[] }) {
+	private static parseActions(actors: { [creepName: string]: CombatZerg },
+								serializedActions: { [creepName: string]: RLAction[] }) {
+
+		const receivedOrders: { [creepName: string]: boolean } = _.mapValues(actors, actor => false);
+
+		// Deserialize the actions for each actor
 		for (const creepName in serializedActions) {
-			const creep = Game.creeps[creepName];
+
+			const creep = actors[creepName] as CombatZerg | undefined;
+
 			if (!creep) {
 				console.log(`No creep with name ${creepName}!`);
-			} else {
-				for (const action of serializedActions[creepName]) {
-					const [command, id] = action;
-					const targ: RoomObject | null = typeof id == 'string' ? Game.getObjectById(id) : null;
-					switch (command) {
-						case 'move':
-							creep.move(<DirectionConstant>id);
-							break;
-						case 'moveTo':
-							if (targ) creep.moveTo(targ);
-							break;
-						case 'attack':
-							if (targ) creep.attack(<Creep>targ);
-							break;
-						case 'rangedAttack':
-							if (targ) creep.rangedAttack(<Creep>targ);
-							break;
-						case 'rangedMassAttack':
-							creep.rangedMassAttack();
-							break;
-						case 'heal':
-							if (targ) {
-								creep.heal(<Creep>targ);
-							} else if (typeof id != 'string') {
-								creep.heal(creep);
-							}
-							break;
-						case 'rangedHeal':
-							if (targ) creep.rangedHeal(<Creep>targ);
-							break;
-						default:
-							console.log(`Invalid command: ${command}!`);
-							break;
-					}
+				continue;
+			}
+
+			// Parse and execute each action, recording whether it was valid
+			for (const action of serializedActions[creepName]) {
+				const validAction = ActionParser.parseAction(creep, action);
+				if (validAction) {
+					receivedOrders[creepName] = true;
 				}
+			}
+
+		}
+
+		// Ensure each actor was given an order (possibly noop)
+		for (const actorName in actors) {
+			if (!receivedOrders[actorName]) {
+				console.log(`[${Game.time}] Actor with name ${actorName} did not receive an order this tick!`);
 			}
 		}
 	}
@@ -77,8 +125,8 @@ export class ActionParser {
 	/**
 	 * Wraps all creeps as Zerg
 	 */
-	private static wrapZerg(useCombatZerg = true) {
-
+	private static wrapZerg(): { [creepName: string]: CombatZerg } {
+		return _.mapValues(Game.creeps, creep => new CombatZerg(creep));
 	}
 
 	/**
@@ -86,11 +134,14 @@ export class ActionParser {
 	 */
 	static run() {
 
+		const actors = ActionParser.wrapZerg();
 		const raw = RawMemory.segments[RL_ACTION_SEGMENT];
 
 		if (raw != undefined && raw != '') {
 			const actions = JSON.parse(raw);
-			ActionParser.parseActions(actions);
+			ActionParser.parseActions(actors, actions);
+		} else {
+			console.log(`[${Game.time}]: No actions received!`);
 		}
 
 		RawMemory.setActiveSegments([RL_ACTION_SEGMENT]); // keep this segment requested during training
